1ca18fdbf33e5d6afcb6f93eaeb1e8b0ad659e64
student <student@COMP-209>
2021-02-14 16:20:11 +0200

    sdfsd

--- TanksServer/Program.cs
+++ TanksServer/Program.cs
@@ -1,5 +1,15 @@
 ﻿
 using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
 
 namespace ChatServer
 {
@@ -79,6 +89,39 @@ namespace ChatServer
         {
             Stream?.Close();
             tcpClient.Close();
+        }
+    }
+    class Server : IDisposable
+    {
+        public List<Client> Clients { get; set; }
+        private TcpListener listener;
+        public Server(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            Clients = new List<Client>();
+        }
+        public void Start()
+        {
+            listener.Start();
+            Console.WriteLine("Server started.");
+
+            while (true)
+                new Thread(new ThreadStart(new Client(listener.AcceptTcpClient(), this).Process)).Start();
+        }
+        public void Broadcast(string message, string name)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            foreach (Client client in Clients)
+                if (client.UserName != name)
+                    client.Stream.Write(data, 0, data.Length);
+        }
+        
+        public void Dispose()
+        {
+            listener.Stop();
+            foreach (Client client in Clients)
+                client.Dispose();
+            Console.WriteLine("Server stoped.");
         }
     }
 }
